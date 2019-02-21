bd166b6212533d6dbe5e964ffa5de0c9d0695f3b
Nishant Thapa <nishantthapa@icloud.com>
2019-02-21 23:29:03 +0530

    Updates

---
+++ precache-manifest.f9ea78a0997aab6be1e50c77865a92c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "748e567c8dbdbe609b44",
+    "url": "/Sticky-Note/static/js/runtime~main.748e567c.js"
+  },
+  {
+    "revision": "40511ec1d6a4454d8a4d",
+    "url": "/Sticky-Note/static/js/main.40511ec1.chunk.js"
+  },
+  {
+    "revision": "3ab993e38ffdd549720f",
+    "url": "/Sticky-Note/static/js/2.3ab993e3.chunk.js"
+  },
+  {
+    "revision": "40511ec1d6a4454d8a4d",
+    "url": "/Sticky-Note/static/css/main.a539e419.chunk.css"
+  },
+  {
+    "revision": "8dcde71487c320e35e12cb706a975dac",
+    "url": "/Sticky-Note/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Sticky-Note/precache-manifest.f9ea78a0997aab6be1e50c77865a92c9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Sticky-Note/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
